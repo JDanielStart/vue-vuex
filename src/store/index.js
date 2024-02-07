7de10a373508f01494ab3417c2a7df5322c8c29f
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-02-07 22:18:28 +0000

    Getter example

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,12 @@ const store = createStore({
     return {
         username: "J.Daniel"
     }
-  }
+  },
+  getters: {
+    firstName: (state) => (c) => {
+        return state.username.split("").reverse().join(c)
+    }
+  },
 });
 
 export default store;
