67722964452dbd2b50bf298ca23233b8c6ff44e3
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-02-07 23:07:11 +0000

    Example of actions

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,6 @@
 <script>
 
-import { mapState, mapMutations } from 'vuex';
+import { mapState, mapMutations, mapActions } from 'vuex';
 
 export default {
   computed: {
@@ -9,7 +9,7 @@ export default {
     })
   },
   methods: {
-    ...mapMutations(["updateUsername"])
+    ...mapActions(["updateUsername"])
   }
 }
 </script>
