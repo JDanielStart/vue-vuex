d1e1cb6354015e6e0a0abb0b2fe9cd1e143eac2d
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-02-08 19:31:07 +0000

    data has been normalized

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,50 @@
-import { reactive } from "vue";
+const store = {
+    profile: {
+        username: ""
+    },
+    contacts: [
+        {
+            id: 1,
+            username: "User 1"
+        },
+        {
+            id: 2,
+            username: "User 1"
+        }
+    ],
+    messages: [
+        {
+            id: 1,
+            msg: "Hola 😀",
+            author: 1
+        },
+        {
+            id: 2,
+            msg: "Hola 😀",
+            author: 2
+        },
+        {
+            id: 3,
+            msg: "Hola 😀",
+            author: 1
+        },
+        {
+            id: 4,
+            msg: "Hola 😀",
+            author: 2
+        }
 
-const store = reactive({
-    username: "diana.nerd",
-    updateUsername(username) {
-        this.username = username
-    }
-})
-
-export default store
+    ],
+    channels: [
+        {
+            id: 1,
+            name: "Canal 1",
+            messages: [1, 2]
+        },
+        {
+            id: 2,
+            name: "Canal 2",
+            messages: [3, 4]
+        }
+    ]
+}
